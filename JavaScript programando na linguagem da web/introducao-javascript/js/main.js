fdbbe8b1ad084f6cf87e36cbef8ea565180b1e08
p4kz <wolpak@hotmail.com>
2022-04-14 08:11:18 -0300

    create for and color error in table

--- JavaScript programando na linguagem da web/introducao-javascript/js/main.js
+++ JavaScript programando na linguagem da web/introducao-javascript/js/main.js
@@ -1,30 +1,38 @@
-let paciente = document.querySelector('.paciente')
+let pacientes = document.querySelectorAll('.paciente')
+ 
 
-let tdPeso = paciente.querySelector('.info-peso')
-let peso = tdPeso.textContent
+for ( let i = 0 ; i < pacientes.length ; i++) {
+  paciente = pacientes[i]
 
-let tdAltura = document.querySelector('.info-altura')
-let altura = tdAltura.textContent
+  let tdPeso = paciente.querySelector('.info-peso')
+  let peso = tdPeso.textContent
+  
+  let tdAltura = paciente.querySelector('.info-altura')
+  let altura = tdAltura.textContent
+  
+  let tdImc = paciente.querySelector('.info-imc')
+  
+  // isValid
+  let pesoEValido = true
+  let alturaEValido = true 
+  
+  // Validando Peso / Altura 
+  if (peso <= 0 || peso >= 400) {
+    console.log('Peso invalido')
+    pesoEValido = false
+    tdPeso.textContent = 'Peso invalido'
+    paciente.classList.add('paciente-invalido') 
+  } else if (altura <= 0 || altura >= 2.50) {  
+    console.log('Altura invalida')
+    alturaEValido = false
+    tdAltura.textContent = 'Altura invalida'
+    paciente.classList.add('paciente-invalido') 
+  }
+  
+  // Validando imc
+  if (alturaEValido && pesoEValido) {
+    let imc = peso / (altura * altura) 
+    tdImc.textContent = imc.toFixed(2)
+  }
 
-let tdImc = paciente.querySelector('.info-imc')
-
-// isValid
-let pesoEValido = true
-let alturaEValido = true 
-
-// Validando Peso / Altura 
-if (peso <= 0 || peso >= 400) {
-  console.log('Peso invalido')
-  pesoEValido = false
-  tdPeso.textContent = 'Peso invalido'
-} else if (altura <= 0 || altura >= 2.50) {  
-  console.log('Altura invalida')
-  alturaEValido = false
-  tdAltura.textContent = 'Altura invalida'
-}
-
-// Validando imc
-if (alturaEValido && pesoEValido) {
-  let imc = peso / (altura * altura) 
-  tdImc.textContent = imc
 }
