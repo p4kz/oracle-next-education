0810da9ad747052e0284fe1dc94c2356f3cc9572
p4kz <wolpak@hotmail.com>
2022-04-15 10:24:25 -0300

    create danamic table

--- JavaScript programando na linguagem da web/introducao-javascript/js/main.js
+++ JavaScript programando na linguagem da web/introducao-javascript/js/main.js
@@ -34,5 +34,43 @@ for ( let i = 0 ; i < pacientes.length ; i++) {
     let imc = peso / (altura * altura) 
     tdImc.textContent = imc.toFixed(2)
   }
-
 }
+
+// Add pessoas
+let botaoAdcionar = document.querySelector('#adicionar-paciente')
+
+botaoAdcionar.addEventListener('click', () => {
+  event.preventDefault()
+
+  // Pegando dados 
+  let form = document.querySelector('#form-adiciona')
+  let nome = form.nome.value
+  let peso = form.peso.value
+  let altura = form.altura.value
+  let gordura = form.gordura.value
+
+  // criando paciente no html
+  let pacienteTr = document.createElement('tr')
+
+  let nomeTd = document.createElement('td')
+  let pesoTd = document.createElement('td')
+  let alturaTd = document.createElement('td')
+  let gorduraTd = document.createElement('td')
+  let imcTd = document.createElement('td')
+
+  nomeTd.textContent = nome
+  pesoTd.textContent = peso
+  alturaTd.textContent = altura
+  gorduraTd.textContent = gordura
+  
+  // Colocando elementos dentro do pacienteTr
+  pacienteTr.appendChild(nomeTd)
+  pacienteTr.appendChild(pesoTd)
+  pacienteTr.appendChild(alturaTd)
+  pacienteTr.appendChild(gorduraTd)
+
+  // Colocando pacienteTr dentro da tabela 
+  let tabela = document.querySelector('#tabela-pacientes')
+  tabela.appendChild(pacienteTr)
+
+})
