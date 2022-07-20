67df659d62382c45e645cfd29baf096f70abcd1e
p4kz <wolpak@hotmail.com>
2022-07-20 10:00:58 -0300

    custom mensages - validate

--- 02 - Front End F2T2 - ONE/JS na Web - Validação de formulário/js/validacao.js
+++ 02 - Front End F2T2 - ONE/JS na Web - Validação de formulário/js/validacao.js
@@ -1,14 +1,61 @@
 export function valida(input) {
-  const tipoDeinput = input.dataset.tipo
+  const tipoDeInput = input.dataset.tipo
 
-  if(Validadores[tipoDeinput]){
-    Validadores[tipoDeinput](input)
+  if(validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input)
+  }
+
+  if(input.validity.valid) {
+    input.parentElement.classList.remove('input-container--invalido')
+    input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+  } else {
+    input.parentElement.classList.add('input-container--invalido')
+    input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
   }
 }
 
-const Validadores = {
+const tiposDeErro = [
+  'valueMissing',
+  'typeMismatch',
+  'patternMismatch',
+  'customError'
+]
+
+const mensagensDeErro = {
+  nome: {
+    valueMissing: 'O campo nome não pode estar vazio',
+  },
+  email: {
+    valueMissing: 'O campo email não pode estar vazio',
+    typeMismatch: 'O email digitado não é valido'
+  },
+  senha: {
+    valueMissing: 'O campo senha não pode estar vazio',
+    patternMismatch: 'A senha deve conter entre 6 e 12 caracteres, deve conter pelomenos uma letra maiuscula, um numero sem simbolos'
+  },
+  dataNascimento:  {
+    valueMissing: 'O campo data não pode estar vazio',
+    customError: 'Você deve ser maior que 18 anos para se cadastrar'
+  }
+}
+
+const validadores = {
   dataNascimento: input => validaDataNacimento(input), 
 } 
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+  let mensagem = ''
+
+  tiposDeErro.forEach(erro => {
+    if(input.validity[erro]) {
+      mensagem = mensagensDeErro[tipoDeInput][erro]
+    }
+  })
+
+  
+  
+  return mensagem
+}
 
 function validaDataNacimento(input) {
   const dataRecebida = new Date(input.value)
