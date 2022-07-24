4c81ed8f70ed184a214c6d57c3062ec461acdc64
p4kz <wolpak@hotmail.com>
2022-07-24 16:44:53 -0300

    end module - explorer

--- 02 - Front End F2T2 - ONE/JavaScript - Explorando a linguagem/index.js
+++ 02 - Front End F2T2 - ONE/JavaScript - Explorando a linguagem/index.js
@@ -7,3 +7,17 @@ console.log(( 10 + 8 ) * 2)
 let seila = parseInt('2') + parseInt('2')
 
 console.log(`hello ${seila} hellohello`)
+
+const idadeValidate = (idade, acompanhado) => {
+  if (idade < 18 && acompanhado === false) {
+    console.log('Menores de idade não acompanhados não podem viajar')
+    return
+  } else if (idade < 18 && acompanhado === true) {
+    console.log('Menores de idade acompanhados podem viajar')
+    return
+  } else {
+    console.log('Pode viajar')
+  } 
+} 
+
+idadeValidate(18, true)
