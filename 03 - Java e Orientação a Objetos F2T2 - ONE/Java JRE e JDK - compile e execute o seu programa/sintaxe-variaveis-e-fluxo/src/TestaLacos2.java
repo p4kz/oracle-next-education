ce7292e5f35da59f8984aab1e7f15ebe763d4268
p4kz <wolpak@hotmail.com>
2022-08-15 17:11:35 -0300

    end module - java

---
+++ 03 - Java e Orientação a Objetos F2T2 - ONE/Java JRE e JDK - compile e execute o seu programa/sintaxe-variaveis-e-fluxo/src/TestaLacos2.java
@@ -0,0 +1,14 @@
+
+public class TestaLacos2 {
+
+	public static void main(String[] args) {
+		for (int linha = 0; linha < 10; linha++) {
+			for (int col = 0; col <= linha; col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
