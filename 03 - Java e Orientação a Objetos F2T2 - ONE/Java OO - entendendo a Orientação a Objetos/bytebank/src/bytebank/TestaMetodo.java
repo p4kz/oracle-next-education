bebc986e06976b5d2ecfee3021da13d5ad1e231b
p4kz <wolpak@hotmail.com>
2022-08-18 00:11:50 -0300

    intro methods - java oo

---
+++ 03 - Java e Orientação a Objetos F2T2 - ONE/Java OO - entendendo a Orientação a Objetos/bytebank/src/bytebank/TestaMetodo.java
@@ -0,0 +1,26 @@
+package bytebank;
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta contaEric = new Conta();
+		contaEric.saldo = 100;
+		contaEric.deposita(50);
+		contaEric.saca(20);
+		
+		Conta contaMarco = new Conta();
+		contaMarco.saldo = 1000;
+		
+		boolean sucessoTransferencia = contaMarco.transfere(300, contaEric);
+		
+		System.out.println(contaEric.saldo);
+		System.out.println(contaMarco.saldo);
+		
+		if(sucessoTransferencia) {
+			System.out.println("Tranferencia feita com sucesso");
+		} else {
+			System.out.println("Saldo insuficiente");
+		}
+	}
+
+}
