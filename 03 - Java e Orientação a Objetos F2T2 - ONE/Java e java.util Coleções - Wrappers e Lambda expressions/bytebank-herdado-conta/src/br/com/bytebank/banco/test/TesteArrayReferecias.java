542a8f4b0dae857279ad77ea56319f460ef20af9
p4kz <wolpak@hotmail.com>
2022-09-09 05:15:47 -0300

    intro arrays - java util

--- 03 - Java e Orientação a Objetos F2T2 - ONE/Java e java.util Coleções - Wrappers e Lambda expressions/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferecias.java
+++ 03 - Java e Orientação a Objetos F2T2 - ONE/Java e java.util Coleções - Wrappers e Lambda expressions/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferecias.java
@@ -1,11 +1,13 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPopanca;
 
 public class TesteArrayReferecias {
 
 	public static void main(String[] args) {
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] contas = new Object[5];
 		
 		ContaCorrente cc1 = new ContaCorrente(11, 11);
 		contas[0] = cc1;
@@ -13,11 +15,10 @@ public class TesteArrayReferecias {
 		ContaCorrente cc2 = new ContaCorrente(22, 22);
 		contas[1] = cc2;
 		
-		for (int i = 0; i < contas.length; i++) {
-	
-		}
-		
-		System.out.println( contas[0].getAgencia());
+		ContaPopanca cc3 = new ContaPopanca(33, 33);
+		contas[2] = cc3;
+			
+		System.out.println( ((Conta) contas[2]).getAgencia());
 
 	}
 
