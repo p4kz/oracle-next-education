f7b7d7f373d3e6ce809c99c7e7909db676dbac03
p4kz <wolpak@hotmail.com>
2022-09-20 05:34:33 -0300

    end module - java util

--- 03 - Java e Orientação a Objetos F2T2 - ONE/Java e java.util Coleções - Wrappers e Lambda expressions/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/TesteSort.java
+++ 03 - Java e Orientação a Objetos F2T2 - ONE/Java e java.util Coleções - Wrappers e Lambda expressions/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/TesteSort.java
@@ -6,48 +6,36 @@ import java.util.List;
 
 public class TesteSort {
 
-        public static void main(String[] args) {
+	public static void main(String[] args) {
 
-                Conta cc1 = new ContaCorrente(22, 33);
-                cc1.deposita(333.0);
+		Conta cc1 = new ContaCorrente(22, 33);
+		cc1.deposita(333.0);
 
-                Conta cc2 = new ContaCorrente(22, 44);
-                cc2.deposita(444.0);
+		Conta cc2 = new ContaCorrente(22, 44);
+		cc2.deposita(444.0);
 
-                Conta cc3 = new ContaCorrente(22, 11);
-                cc3.deposita(111.0);
+		Conta cc3 = new ContaCorrente(22, 11);
+		cc3.deposita(111.0);
 
-                Conta cc4 = new ContaCorrente(22, 22);
-                cc4.deposita(222.0);
+		Conta cc4 = new ContaCorrente(22, 22);
+		cc4.deposita(222.0);
 
-                List<Conta> lista = new ArrayList<Conta>();
-                lista.add(cc1);
-                lista.add(cc2);
-                lista.add(cc3);
-                lista.add(cc4);
-                
-                NumeroDaContaComparator comparador = new NumeroDaContaComparator();
-                lista.sort(comparador);
-                
-                System.out.println("-----------------");
-                for (Conta conta : lista) {
-                	System.out.println(conta);
-                }
+		List<Conta> lista = new ArrayList<Conta>();
+		lista.add(cc1);
+		lista.add(cc2);
+		lista.add(cc3);
+		lista.add(cc4);
 
-        }
+		lista.sort((c1, c2) -> Integer.compare(c1.getNumero(), c2.getNumero()));
 
-}
+		Comparator<Conta> comp = (Conta c1, Conta c2) -> {
+			String nomeC1 = c1.getTitular().getNome();
+			String nomeC2 = c2.getTitular().getNome();
+			return nomeC1.compareTo(nomeC2);
+		};
 
-class NumeroDaContaComparator implements Comparator<Conta> {
-	@Override
-	public int compare(Conta c1, Conta c2) {
-		if (c1.getNumero() > c2.getNumero()) {
-			return -1;
-		} 
-		
-		if (c1.getNumero() > c2.getNumero()) {
-			return 1;
-		} 
-		return 0;
+		lista.forEach((conta) -> System.out.println(conta + ", " + conta.getTitular().getNome()));
+
 	}
+
 }
