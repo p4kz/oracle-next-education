e38af5d6919615717bea61d421aa6f932e32ea26
p4kz <wolpak@hotmail.com>
2022-10-03 16:18:58 -0300

    linkedlist - java collections

---
+++ 03 - Java e Orientação a Objetos F2T2 - ONE/Java Collections - Dominando Listas, Sets e Mapas/gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,19 @@
+package br.com.alura;
+
+public class TestaCurso {
+
+	public static void main(String[] args) {
+	    Curso javaColecoes = new Curso("Dominando as colecoes do Java", "Paulo Silveira");
+	
+	    javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+	    javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+	    javaColecoes.adiciona(new Aula("Modelando com colecoes", 24));
+	
+	    // tentando adicionar da maneira "antiga". Podemos fazer isso? Teste:
+//	    javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
+	
+	    System.out.println(javaColecoes.getAulas());
+
+	}
+
+}
