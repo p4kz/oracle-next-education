26101fce1c7ccf189e01002d86e3718e41a2d5bd
p4kz <wolpak@hotmail.com>
2022-11-30 14:04:03 -0300

    intro loops - py basic

--- 04 - AlumniONE/Python, Data Science no OCI e Oracle Analytics - ONE/Python para Data Science/basic.py
+++ 04 - AlumniONE/Python, Data Science no OCI e Oracle Analytics - ONE/Python para Data Science/basic.py
@@ -1,20 +1,37 @@
-nome = 'Eric'
-idade = 24
+# nome = 'Eric'
+# idade = 24
 
 # print ( nome, idade)
 
 # ##template string
 # print (f'meu nome é {nome} e tenho {idade} anos')
 
-# ##function
+##function
 # def saudacao(nome_da_pessoa):
 #   nome = input('qual seu nome?')
 #   print(f'Olá, {nome_da_pessoa}')
 # saudacao(nome)
 
 ##conditional
-def verifica_idade(idade):
-  if idade >= 18:
-    print('tem permissão para dirigir')
-verifica_idade(idade)
+# def verifica_idade(idade):
+#   if idade >= 18:
+#     print('tem permissão para dirigir')
+# verifica_idade(idade)
+
+#array
+idades = [15, 16, 17, 18]
+
+##loop
+def verifica_se_pode_dirigir(idade):
+  if idade >=18:
+    print(f'{idade} anos de idade, tem permissão para dirigir')
+  else: 
+    print(f'{idade} anos de idade, não tem permissão para dirigir')
+
+for idade in idades:
+  verifica_se_pode_dirigir(idade)
+  
+
+ 
+
 
--- 04 - AlumniONE/Python, Data Science no OCI e Oracle Analytics - ONE/Python para Data Science/roadmap.md
+++ 04 - AlumniONE/Python, Data Science no OCI e Oracle Analytics - ONE/Python para Data Science/roadmap.md
@@ -6,3 +6,7 @@
 - Criamos uma função que recebe um parâmetro;
 - Aprendemos a usar as condicionais if e else;
 - Convertemos a string (str) digitando através do teclado em um valor inteiro (int).
+- Criamos nossa primeira lista no Python;
+- Aprendemos a utilizar a estrutura de repetição for;
+- Descobrimos o tipo booleano.
+
